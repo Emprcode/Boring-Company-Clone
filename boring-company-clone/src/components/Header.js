732e70a5ed1be3f0fdb29fdfdfd8777856913e38
Narayan Aryal <narayanaryal.code@gmail.com>
2023-06-19 23:15:28 +1000

    top page completed

--- boring-company-clone/src/components/Header.js
+++ boring-company-clone/src/components/Header.js
@@ -33,16 +33,22 @@ export const Header = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-<Row className='p-3 mt-5 d-flex align-items-end'>
-    <h5>
+<Row className='info'>
+  <div className='information'>
+<div>
+    <h5 className='mb-4 fw-bold'>
 
     
     The Boring Company creates safe, fast-to-dig, and low-cost transportation, utility, and freight tunnels
     </h5>
-    <h5>
+  
+   
+    <h5 className='fw-bold'>
 
 The mission: solve traffic, enable rapid point-to-point transportation and transform cities
     </h5>
+    </div>
+    </div>
 </Row>
 
     </div>
